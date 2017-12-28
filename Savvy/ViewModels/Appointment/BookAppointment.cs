83b3fcd315ae66b73b7ecaef8bebdabf3a1b1e69
lsimmons1832 <lsimmons1832@gmail.com>
2017-12-28 17:04:54 -0600

    save progress

--- Savvy/ViewModels/Appointment/BookAppointment.cs
+++ Savvy/ViewModels/Appointment/BookAppointment.cs
@@ -8,8 +8,9 @@ namespace Savvy.ViewModels.Appointment
 {
     public class BookAppointment
     {
+        public int AppointmentId { get; set; }
         public SelectList Stylist { get; set; }
-        public int MyStylist { get; set; }
+        //public int MyStylist { get; set; }
         public int Services { get; set; }
         public DateTime Schedule { get; set; }
         public int CustomerId { get; set; }
